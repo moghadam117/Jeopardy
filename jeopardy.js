05fda719ee54c5943de168deb453315988517402
moghadam117 <saman.moghadam@gmail.com>
2021-03-01 11:48:22 -0800

    completed

---
+++ jeopardy.js
@@ -0,0 +1,34 @@
+
+window.onload=() => {
+    retrieveQuestion();
+    document.getElementById("reveal").onclick=reveal;
+    document.getElementById("next").onclick=retrieveQuestion;
+};
+
+var state = {
+    isLoaded: false
+}
+function retrieveQuestion() {
+    document.getElementById("qdiv").style.display='none';
+    var c = document.getElementById("category");
+    var q = document.getElementById("question");
+    var a = document.getElementById("answer");
+    c.innerHTML="Loading";
+
+    a.innerHTML="Loading";
+fetch('http://jservice.io/api/random').then( (response) => response.json()).then( (data) => {
+    
+    
+    
+    c.innerHTML=data[0].category.title;
+    q.innerHTML=data[0].answer;
+    a.innerHTML=data[0].question;
+
+})}
+function parseJeopardy (){
+
+}
+
+function reveal () {
+    document.getElementById("qdiv").style.display='block';
+}
